5b1641bbdce3c72b5acdbaefd002fa1c583825f6
miaowing <me@zf.ink>
2020-07-15 15:09:54 +0800

    fix webview always blank page

--- src/modules/discovery/WebView.tsx
+++ src/modules/discovery/WebView.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Layout } from "../../components/Layout";
+import * as styles from './WebView.module.less';
 
 export class WebView extends React.Component<any> {
     render() {
+        console.log(this.props.location.search.replace('?url=', ''));
         return <Layout>
-            <webview id="web"
-                     src={ this.props.location.search.replace('?url=', '') }
-                     style={ { height: 524, margin: '0 -30px -30px -30px' } }/>
+            <iframe className={styles.iframe}
+                    id="web"
+                    src={this.props.location.search.replace('?url=', '')}/>
         </Layout>;
     }
 }
