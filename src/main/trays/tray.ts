7d21948e3577781be676d44fe8e35f2b2e007bdc
gavinzhao <gavinzhao@futunn.com>
2022-09-13 18:13:06 +0800

    fix: js error

--- src/main/trays/tray.ts
+++ src/main/trays/tray.ts
@@ -18,7 +18,6 @@ export const initTray = (window: BrowserWindow) => {
 
     trayObj.on('click', e => {
         if (!window.isVisible()) {
-            (e as any).preventDefault();
             window.show();
         } else {
             window.setAlwaysOnTop(true);
--- src/modules/auth/auth.container.tsx
+++ src/modules/auth/auth.container.tsx
@@ -3,7 +3,7 @@ import { Form, Input, Button } from 'antd';
 import type { FormInstance } from 'antd/es/form';
 import * as styles from './auth.m.less';
 import { instance } from '../../helpers';
-import { LoadingOutlined, LockOutlined, UserOutlined } from "@ant-design/icons";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 
 export class Auth extends React.Component<any, any> {
     formRef = React.createRef<FormInstance>();
@@ -15,8 +15,7 @@ export class Auth extends React.Component<any, any> {
 
     private timer;
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
+    handleSubmit = async () => {
         await this.formRef.current!.validateFields();
         const values = this.formRef.current!.getFieldsValue();
         this.setState({ error: false, loading: true });
@@ -32,42 +31,43 @@ export class Auth extends React.Component<any, any> {
             }
             this.timer = setTimeout(() => this.setState({
                 error: false,
-            }), 1000);
+            }), 5000);
         } else {
             this.setState({ error: false, loading: false });
         }
     };
 
     render() {
-        const { getFieldDecorator } = this.props.form;
         return <div className={styles.auth}>
-            <Form onFinish={this.handleSubmit} ref={this.formRef} className={styles.login_form}>
-                <Form.Item>
-                    {getFieldDecorator('username', {
-                        rules: [{ required: true, message: '请输入通行证/邮箱/手机号码' }],
-                    })(
-                        <Input prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>}
-                               placeholder="通行证/邮箱/手机号码"/>
-                    )}
+            <Form onFinish={() => this.handleSubmit()} className={styles.login_form} ref={this.formRef}>
+                <Form.Item rules={[{ required: true, message: '请输入通行证/邮箱/手机号码' }]} name="username">
+                    <Input
+                        prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                        placeholder="通行证/邮箱/手机号码"
+                    />
+                </Form.Item>
+                <Form.Item rules={[{ required: true, message: '请输入密码' }]} name="password">
+                    <Input
+                        prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                        type="password"
+                        placeholder="密码"
+                    />
                 </Form.Item>
                 <Form.Item>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: '请输入密码' }],
-                    })(
-                        <Input prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>} type="password"
-                               placeholder="密码"/>
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    <span style={{ color: '#f00' }}>{this.state.error ? this.state.error : ''}</span>
+                    <span style={{ color: '#f00' }}>
+                        {this.state.error ? this.state.error : ''}
+                    </span>
                     <a className={styles.login_form_forgot} href="">忘记密码</a>
-                    <Button disabled={this.state.loading}
-                            type="primary" htmlType="submit" className={styles.login_form_button}>
-                        {this.state.loading ? <LoadingOutlined/> : ''}登录{this.state.loading ? '中...' : ''}
+                    <Button
+                        loading={this.state.loading}
+                        type="primary"
+                        htmlType="submit"
+                        className={styles.login_form_button}>
+                        登录{this.state.loading ? '中...' : ''}
                     </Button>
                     <a href="">立即注册</a>
                 </Form.Item>
             </Form>
-        </div>;
+        </div>
     }
 }
